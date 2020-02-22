a5239aea09d9195f02412b1a7bc34deeb0def75d
BacNguyen <luongbac301092@gmail.com>
2020-02-22 11:00:30 +0700

    update

--- pipeline-scripts/update_pipeline.py
+++ pipeline-scripts/update_pipeline.py
@@ -84,7 +84,7 @@ if __name__ == "__main__":
     print(len(sys.argv))
     if len(sys.argv) == 9:
         update_pipeline(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5], sys.argv[6], sys.argv[7], sys.argv[8])
-     elif len(sys.argv) == 10:
+    elif len(sys.argv) == 10:
         update_pipeline(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5], sys.argv[6], sys.argv[7], sys.argv[8], sys.argv[9], None)
     elif len(sys.argv) == 11:
         update_pipeline(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5], sys.argv[6], sys.argv[7], sys.argv[8], sys.argv[9], sys.argv[10])
